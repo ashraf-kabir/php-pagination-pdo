579b97bffe17cbf213d2400dd5cb24311d0fe73a
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-14 10:42:04 +0600

    indentation fixed

--- data.php
+++ data.php
@@ -2,33 +2,33 @@
 include 'db.php';
 ?>
 
-<?php 
+<?php
 // For Slecting from tabel name posts
 $sql = "SELECT*FROM posts";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
- // Check If it exist
-    // $posts = $stmt->fetchAll();
-    //  foreach($posts as $post){
-    //      echo $post->title;
-    //  } 
+// Check If it exist
+// $posts = $stmt->fetchAll();
+//  foreach($posts as $post){
+//      echo $post->title;
+//  }
 // Result per page you want
 $result_per_page = 3;
 // Number of results
 $number_of_results = $stmt->rowCount();
 // Number of pages
-$number_of_pages = ceil($number_of_results/$result_per_page);
+$number_of_pages = ceil($number_of_results / $result_per_page);
 // Check will be from ajax
 if (isset($_POST['page'])) {
     $page = $_POST['page'];
-} else{
+} else {
     $page = 1;
 }
 // Determine Limit
-$this_page_first_result = ($page-1)*$result_per_page;
+$this_page_first_result = ($page - 1) * $result_per_page;
 // Finally Showing data
 
-$sql = "SELECT * FROM posts ORDER BY `id` LIMIT ".$this_page_first_result.','.$result_per_page;
+$sql = "SELECT * FROM posts ORDER BY `id` LIMIT " . $this_page_first_result . ',' . $result_per_page;
 //var_dump($sql);
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
@@ -36,26 +36,26 @@ $posts = $stmt->fetchAll(PDO::FETCH_OBJ);
 $cnt = 1;
 $output = '';
 if ($stmt->rowCount() > 0) {
-    foreach($posts as $post){ ?>
+    foreach ($posts as $post) {?>
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Body</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?php echo htmlentities($post->title); ?></td>
-                            <td><?php echo htmlentities($post->author); ?></td>
-                            <td><?php echo htmlentities($post->body); ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Body</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?php echo htmlentities($post->title); ?></td>
+                                <td><?php echo htmlentities($post->author); ?></td>
+                                <td><?php echo htmlentities($post->body); ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -63,16 +63,16 @@ if ($stmt->rowCount() > 0) {
     }
 }
 // Pagination link
-for ($i=max(1,$page-2); $i<= min($page+4,$number_of_pages);$i++) {
-    if ($i== $page) {
-        echo '<a style="cursor:pointer" class="pagination-link page-active" id="'.$i.'">'.$i.'</a>';
+for ($i = max(1, $page - 2); $i <= min($page + 4, $number_of_pages); $i++) {
+    if ($i == $page) {
+        echo '<a style="cursor:pointer" class="pagination-link page-active" id="' . $i . '">' . $i . '</a>';
     } else {
-        echo '<a style="cursor:pointer" class="pagination-link" id="'.$i.'">'.$i.'</a>';
+        echo '<a style="cursor:pointer" class="pagination-link" id="' . $i . '">' . $i . '</a>';
     }
 }
 $check = $this_page_first_result + $result_per_page;
 $next = $page + 1;
 if ($number_of_results > $check) {
-    echo '<a class="pagination-link" style="cursor:pointer" id="'.$next.'">Next</a>';
+    echo '<a class="pagination-link" style="cursor:pointer" id="' . $next . '">Next</a>';
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <title>PHP PDO PAGINATION TEST + Auto Refresh Content</title>
     <!-- Required meta tags -->
@@ -7,62 +8,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-</head>
-<body>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+        integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
     <!-- CSS for Page Never use like this it is only for test purpose -->
     <style>
-       body,html{
-          width:100%;
-          height:100%;
-       }
-       h1{
-           font-size:1.2rem;
-           margin-bottom:2rem;
-           text-align:center;
-       }
-       a.pagination-link{
-           display:inline-block;
-           text-align:center;
-       }
-       .page-active{
-           background:tomato;
-       }
+    body,
+    html {
+        width: 100%;
+        height: 100%;
+    }
+
+    h1 {
+        font-size: 1.2rem;
+        margin-bottom: 2rem;
+        text-align: center;
+    }
+
+    a.pagination-link {
+        display: inline-block;
+        text-align: center;
+    }
+
+    .page-active {
+        background: tomato;
+    }
     </style>
+
+</head>
+
+<body>
+
     <h1>Pagination Test using ajax + auto refesh Content</h1>
     <!-- Here the Data will be loaded using ajax from data.php -->
     <div id="load_data"></div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"
+        integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous">
+    </script>
     <script>
-       // Using Ajax to load Data
-      $(document).ready(function(){
-         setTimeout(() => {
-              load_fn_data();
-         }, 1000);
-      });
-      
-      function load_fn_data(page){
-          $.ajax({
-             url:"data.php",
-             method:"POST",
-             data:{
-                page:page
-             },
-             success:function(data){
-                 $('#load_data').html(data);
-             }
-          });
-      }
+    // Using Ajax to load Data
+    $(document).ready(function() {
+        setTimeout(() => {
+            load_fn_data();
+        }, 1000);
+    });
 
-      // For Pagination
-      $(document).on('click','.pagination-link',function() {
-          var page = $(this).attr("id");
-          load_fn_data(page);
-      });
+    function load_fn_data(page) {
+        $.ajax({
+            url: "data.php",
+            method: "POST",
+            data: {
+                page: page
+            },
+            success: function(data) {
+                $('#load_data').html(data);
+            }
+        });
+    }
+
+    // For Pagination
+    $(document).on('click', '.pagination-link', function() {
+        var page = $(this).attr("id");
+        load_fn_data(page);
+    });
     </script>
 </body>
+
 </html>
